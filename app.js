9e80295c37ea91f1503f67114c21261bf437ed92
shreya <shreyaniyadav@gmail.com>
2024-12-09 22:52:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, Jenkins CI/CD!');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Server running at http://0.0.0.0:${PORT}');
+});
